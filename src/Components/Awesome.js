8c6b2811e54874527711d2b983df4c3a70701674
mohan2002 <64457034+mohan2002@users.noreply.github.com>
2022-01-11 19:08:15 +0530

    Explores

--- src/Components/Awesome.js
+++ src/Components/Awesome.js
@@ -9,7 +9,7 @@ function Awesome() {
         </div>
 
         <div className='video'>
-            <iframe className='vd' width="100%" height="315" src="https://www.youtube.com/embed/5L4DQfVIcdg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe className='vd' width="100%" height="380" src="https://www.youtube.com/embed/5L4DQfVIcdg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
 
         <div>
